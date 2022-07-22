c22d6e75064f6b3a9ab205705dd06ab3c9040064
shoko <shoko@shoko.co.il>
2022-07-22 20:03:47 +0300

    S-001-fixed-1

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+
+
+#EXAMPLE:
+# ~/exercise6-fix.sh ~/.profile /tmp
+
+
+UploadDir=$1
+RemoteDir=$2
+
+hostname=$(hostname)
+SSHKEY="/home/vagrant/.ssh/shoko-key"
+
+
+if     [[ "$hostname" == "server1" ]]; then
+		       Host="server2"
+		       	        elif  [[ "$hostname" == "server2" ]]; then
+								       Host="server1"
+								       			        else  echo ERROR
+																		       exit 1
+fi
+
+
+scp -qv  -i $SSHKEY  $UploadDir $UploadDir  vagrant@$Host:$RemoteDir 2>&1 | awk '{if(/Transferred: sent/) print $3}' | sed -e 's/,//g'
+
+
