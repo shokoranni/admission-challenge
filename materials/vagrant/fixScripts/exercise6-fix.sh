7b4dabef17aea6c61890d6c4eeea631b76f0767c
shoko <shoko@shoko.co.il>
2022-07-23 14:23:19 +0300

    S-008 fixed exer6

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -11,16 +11,28 @@ RemoteDir=$2
 hostname=$(hostname)
 SSHKEY="/home/vagrant/.ssh/shoko-key"
 
-
-if     [[ "$hostname" == "server1" ]]; then
-		       Host="server2"
-		       	        elif  [[ "$hostname" == "server2" ]]; then
-								       Host="server1"
-								       			        else  echo ERROR
-																		       exit 1
+if [ ! -f $UploadDir ]
+then
+		echo " ** $UploadDir not exist **"
+		exit 1
 fi
 
 
-scp -qv  -i $SSHKEY  $UploadDir $UploadDir  vagrant@$Host:$RemoteDir 2>&1 | awk '{if(/Transferred: sent/) print $3}' | sed -e 's/,//g'
+if	[[ "$hostname" == "server1" ]]; then
+	Host="server2"
+  elif  [[ "$hostname" == "server2" ]]; then
+	Host="server1"
+  else  echo ERROR
+	exit 1
+fi
 
 
+
+# scp -qv  -i $SSHKEY  $UploadDir  vagrant@$Host:$RemoteDir 2>&1 | awk '{if(/Transferred: sent/) print $3}' | sed -e 's/,//g'
+scp  -i $SSHKEY  $UploadDir  vagrant@$Host:$RemoteDir 2>&1 > /dev/null
+EXIT_STATUS=$?
+if [ $EXIT_STATUS -eq 0 ]; then
+   stat --printf="%s" $UploadDir  
+ else
+      echo "** Some error handling **"
+fi
